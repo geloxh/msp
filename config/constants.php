115545b8117081754cbcd267d58acb1171ddcdec
geloxh <ace.blazado7@gmail.com>
2026-04-15 21:39:23 +0800

    constants

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+    define('ROLE_CHECK_FAILED', 'You are not permitted to do that.');
+    define('PERM_READ', 1);
+    define('PERM_WRITE', 2);
+    define('PERM_FULL', 3);
+    define('MAX_UPLOAD_MB', 500);
+    define('SESSION_LIFETIME', 3600);   
+
