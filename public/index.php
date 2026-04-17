a7fcd77501811c8d300b886c79ea813af8647291
geloxh <ace.blazado7@gmail.com>
2026-04-17 18:07:50 +0800

    wire constants

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
+require __DIR__ . '/../config/constants.php';
+
 session_start();
 
 require __DIR__ . '/../routes/web.php';
